db0fd4b988cfe5142be1466e75fe02f45d7c6f7e
Hui Feng <huifeng248@gmail.com>
2022-05-11 16:20:09 -0700

    linkedlist

--- practice/linked-list-queue-practice.js
+++ practice/linked-list-queue-practice.js
@@ -15,7 +15,9 @@ class SinglyLinkedList {
 
     addToTail(val) {
         let newNode = new SinglyLinkedNode(val);
-        this.length++;
+
+        this.length++
+
         if (!this.head) {
             this.head = newNode;
             return this.head;
@@ -41,25 +43,54 @@ class SinglyLinkedList {
             // }
             // return length + 1;
 
-            return this.length;
+            // return this.length;
+        return this.length; //o(1)
+        // let count = 0
+        // let curr = this.head;
+        // while (curr.next) {
+        //     curr = curr.next;
+        //     count ++
+        // }
+        // count = count +1
+        // return count
     }
 
     sumOfNodes() {
         // Returns the sum of the values of all the nodes
 
         // Write your hypothesis on the time complexity of this method here
+        let sum = 0;
+        let curr = this.head;
+        while (curr.next) {
+            sum += curr.value;
+            curr = curr.next;
+        }
+        return sum += curr.value
     }
 
     averageValue() {
         // Returns the average value of all the nodes
 
         // Write your hypothesis on the time complexity of this method here
+        return this.sumOfNodes() /this.listLength()
     }
 
-    findNthNode(n) {
+    findNthNode(n) { //2
         // Returns the node at the nth index from the head
 
         // Write your hypothesis on the time complexity of this method here
+        let index = 0;
+        let curr = this.head;
+        while (curr.next) {
+            if (index === n) {
+                return curr;
+            } 
+            index ++;
+        
+            curr = curr.next;
+        } 
+
+        return -1
     }
 
     findMid() {
@@ -68,12 +99,50 @@ class SinglyLinkedList {
             // How do the implementation for singly and doubly vary if at all?
 
         // Write your hypothesis on the time complexity of this method here
+        let mid = Math.floor((this.length -1) / 2) //length -1 cause index starts from 0;
+        let index = 0;
+        let curr = this.head;
+        while (curr.next) {
+            if (index === mid) {
+                return curr;
+            }
+            index++;
+            curr = curr.next;
+        }
     }
 
+    addToHead(val) {
+        let newNode = new SinglyLinkedNode(val);
+        this.length++;
+        newNode.next = this.head;
+        this.head = newNode;
+    }
     reverse() {
         // Returns a new reversed version of the linked list
 
         // Write your hypothesis on the time complexity of this method here
+        // let array = []
+        // let curr = this.head;
+        // while (curr.next) {
+        //     array.unshift(curr);
+        //     curr = curr.next;
+        // }
+        // array.unshift(curr.next);
+        // for( let i = 0; i < array.length -1; i++) {
+        //     let ele = array[i];
+        //     ele.next = array[i+1]
+        //     let res = ele;
+        // }
+        // return res;
+        let newList = new SinglyLinkedList()
+        let curr = this.head;
+        while (curr.next) {
+            newList.addToHead(curr.value);
+            curr = curr.next;
+        }
+        newList.addToHead(curr.value);
+        return newList
+
     }
 
     reverseInPlace() {
@@ -95,10 +164,28 @@ class DoublyLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
+
+    addToHead(val) {
+        let newNode = new DoublyLinkedNode(val);
+        this.length++
+
+        if(!this.length) {
+            this.head = newNode;
+            this.tail =newNode;
+            return this.head;
+        }
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+
+    }
+
 
     addToTail(val) {
         let newNode = new DoublyLinkedNode(val);
+        this.length++
 
         if (!this.head) {
             this.head = newNode;
@@ -110,7 +197,7 @@ class DoublyLinkedList {
         newNode.prev = this.tail;
         this.tail = newNode;
 
-        return this.head;
+        return this.head
     }
 
     findMid() {
@@ -119,12 +206,41 @@ class DoublyLinkedList {
             // How do the implementation for singly and doubly vary if at all?
 
         // Write your hypothesis on the time complexity of this method here
+
+        const mid = Math.ceil((this.length -1)/2);
+        let index = 0;
+        let curr = this.tail
+        while (curr.prev) {
+            if (index === mid) {
+                return curr
+            }
+            curr = curr.prev
+            index++
+        } 
+
+
     }
 
     reverse() {
         // Returns a new reversed version of the linked list
 
         // Write your hypothesis on the time complexity of this method here
+        //1 create a list, initializing curr with the head, and while loop through it.
+        //
+
+        const newList = new DoublyLinkedList()
+        let curr = this.head;
+        while(curr.next) {
+            newList.addToHead(curr.value);
+            curr = curr.next;
+        }
+        newList.addToHead(curr.value)
+
+        return newList
+        //2
+
+
+
     }
 
     reverseInPlace() {
@@ -133,6 +249,34 @@ class DoublyLinkedList {
         // Write your hypothesis on the time complexity of this method here
     }
 }
+
+list = new SinglyLinkedList();
+
+// list.addToTail(1);
+// list.addToTail(2);
+// list.addToTail(3);
+// console.log(list.findMid().value)
+
+// list.addToTail(4);
+// list.addToTail(5);
+// list.addToTail(6);
+// console.log(list.findMid().value)
+// console.log("list~~~~~~~",list)
+// list.addToTail(13);
+// list.addToTail(21);
+// // list.addToTail(32);
+// // list.addToTail(14);
+// // list.addToTail(53);
+// list.reverse()
+// console.log("list222222",list)
+
+dll = new DoublyLinkedList();
+// dll.addToTail(1);
+dll.addToTail(2);
+dll.addToTail(3);
+console.log(dll)
+// console.log(dll.findMid())
+
 
 module.exports = {
     SinglyLinkedNode,
